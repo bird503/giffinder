1e3d25b5e4add48e01440c1a61a3132ef0a9110d
Jordan Villafania <jovillafania@s.sfusd.edu>
2018-03-08 01:11:45 +0000

    I worked on everythig and finoished

--- js/script.js
+++ js/script.js
@@ -4,27 +4,33 @@
 
 $(document).ready(function(){
   
-  // WRITE A CLICK HANDLER HERE
-  /*
-    1. click handler function
-    2. get the typed input from the search input box
-    3. call the functions below!
-  */
-  
-  function giphyURLWithSearchTerm(searchTerm) {
-      // write a function that will return a url for the giphy API with
-      // the searchTerm provided in the parameters
-  }
-
-  function appendImageToGallery(srcURL) {
-      // write a function that will append an <img> to the div with class="gallery"
-      // using the URL provided in the parameters
-      
-  }
-
   function callGiphyAPIWithSearchTerm(searchTerm) {
-      // use $.ajax to call the giphy api with the given search term from the parameters.
-      // get the first image url from the ajax response
-      // use appendImageToGallery to put the image onto the screen
-  }
+    // use the giphyURLWithSearchTerm function to customize the url below
+    var giphyURL = giphyURLWithSearchTerm(searchTerm);
+    $.ajax({
+      url: giphyURL,
+      method: "GET",
+      success: function(response) {
+           var url = response.data[0].images.original.url;
+           appendImageToBody(url);
+      },
+    }); 
+}
+
+//Add a click handler below to call the function when the button is clicked
+$("#button0").click(function() {
+callGiphyAPIWithSearchTerm($("#input0").val());
 });
+
+$("#button1").click(function() {
+callGiphyAPIWithSearchTerm($("#input1").val());
+});
+    
+
+$("#randomButton").click(function() {
+     var firstInput = $("#input0").val();
+    var secondInput = $("#input1").val();       
+    var mixedInput = firstInput + " " + secondInput;
+    callGiphyAPIWithSearchTerm(mixedInput);
+    });
+})
